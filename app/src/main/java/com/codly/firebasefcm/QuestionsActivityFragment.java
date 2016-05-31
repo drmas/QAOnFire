cf216c1ecce5447737c8d3ce3d6863a0dc7cebc8
Mohamed Shaban <dr.m.shaban@gmail.com>
2016-05-31 19:46:09 +0200

    First commit

---
+++ app/src/main/java/com/codly/firebasefcm/QuestionsActivityFragment.java
@@ -0,0 +1,22 @@
+package com.codly.firebasefcm;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class QuestionsActivityFragment extends Fragment {
+
+    public QuestionsActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_questions, container, false);
+    }
+}
