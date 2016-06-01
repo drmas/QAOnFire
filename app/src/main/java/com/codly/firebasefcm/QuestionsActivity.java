4b04c8a1587e1c1d0d2bb3ff98250f4a27019c69
Mohamed Shaban <dr.m.shaban@gmail.com>
2016-06-01 19:07:36 +0200

    Firebase Auth

--- app/src/main/java/com/codly/firebasefcm/QuestionsActivity.java
+++ app/src/main/java/com/codly/firebasefcm/QuestionsActivity.java
@@ -1,11 +1,17 @@
 package com.codly.firebasefcm;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class QuestionsActivity extends AppCompatActivity {
 
@@ -26,4 +32,30 @@ public class QuestionsActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_questions, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_sign_out:
+                signout();
+        }
+        return true;
+    }
+
+    private void signout() {
+        FirebaseAuth.getInstance().signOut();
+        openLoginActivity();
+    }
+
+    private void openLoginActivity() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
 }
